d45a3d153ddbdb1b291961d9f9c24dd2ffc4caed
nellerr <55709663+nellerr@users.noreply.github.com>
2019-11-03 13:11:33 +0200

    message

---
+++ script.js
@@ -0,0 +1,24 @@
+// let a = prompt('Введите первое число');
+// let b = prompt('Введите второе число');
+// alert(a*b);
+
+let name = prompt('Имя: ');
+let surname = prompt('Фамилия: ');
+let performance = prompt('Выступление: ');
+let theatr = prompt('Театр: ');
+let sector = prompt('Сектор: ');
+let row = prompt('Ряд: ');
+let place = prompt('Место: ');
+let time = prompt('Время: ');
+
+let order = 'Имя: ' + name;
+ order += 'Фамилия: ' + surname;
+ order += 'Выступление: ' + performance;
+ order += 'Театр: ' + theatr;
+ order += 'Сектор: ' + sector;
+ order += 'Ряд: ' + row;
+ order += 'Место: ' + place;
+ order += 'Время: ' + time;
+ 
+
+console.log(order);
